a4cde82f90548678ed41313e17fe2c2b6dbf1cff
SandzoNight <flukemdc@gmail.com>
2017-10-29 16:50:04 +0700

    with volume control

--- src/Components/Keybox.js
+++ src/Components/Keybox.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import key from './../keymaster'
 import Sound from 'react-sound';
+import InputRange from 'react-input-range';
+import 'react-input-range/lib/css/index.css';
 
 class Keybox extends Component {
   constructor(props) {
@@ -14,13 +16,14 @@ class Keybox extends Component {
       style: ['pressed','paused',''],
       duration: 0,
       position: 0,
+      vol: this.props.vol
     }
   }
 
   render() {
 
     return (
-      <div>
+      <div className="keybox-container">
         <div>
           {this.props.children}
         </div>
@@ -30,21 +33,24 @@ class Keybox extends Component {
             </div>
             <div className="time">{Math.ceil(this.state.position)}/{Math.ceil(this.state.duration)}</div>
         </div>
-          <Sound 
-            url={this.props.sound}
-            playStatus={this.state.status[this.state.statusIndex]}
-            autoLoad={false}
-            onFinishedPlaying={()=>this.stopped()}
-            onPlaying={(val) => this.playing(val)}
-          />
+        <InputRange minValue={0} maxValue={100} value={this.state.vol} onChange={(value)=>{this.setState({vol:value})}}/>
+        <Sound 
+          url={this.props.sound}
+          playStatus={this.state.status[this.state.statusIndex]}
+          autoLoad={false}
+          onFinishedPlaying={()=>this.stopped()}
+          onPlaying={(val) => this.playing(val)}
+          volume={this.state.vol}
+        />
+        
       </div>
     );
   }
   
   pressed() {
-    if(this.state.statusIndex==0){
+    if(this.state.statusIndex===0){
       this.setState({statusIndex:2})
-    }else if(this.state.statusIndex==2||this.state.statusIndex==1){
+    }else if(this.state.statusIndex===2||this.state.statusIndex===1){
       this.setState({statusIndex:0})
     }
   }
